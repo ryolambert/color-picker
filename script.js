5558d2cce6a7aeb5d886ce6643d02452fea4bc18
James R. Lambert <ryolambert@gmail.com>
2018-06-27 16:52:17 -0600

    added/fixed js color picker logic

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function initColorPicker() {
 	let rgb = {
 		red: document.getElementById("red"),
 		green: document.getElementById("green"),
-		blue: document.getElementById("blue"),
+		blue: document.getElementById("blue")
 	};
 	let colorPickers = document.getElementsByClassName("picker");
 	setColorPickerEventListeners(colorBox, rgb, colorPickers);
@@ -15,16 +15,16 @@ function initColorPicker() {
 
 function setColorPickerEventListeners(element, colors, pickerElements) {
 	let pickerLen = pickerElements.length;
-	for (let i = 0; i < pickerLen.length; i++) {
+	for (let i = 0; i < pickerLen; i++) {
 		pickerElements[i].addEventListener('change', () => {
-			console.log("Red value: ", rgb.red.value);
 			let red = colors.red.value;
 			let green = colors.green.value;
-			let blue = colors. blue. value;
+			let blue = colors.blue.value;
 			setElementBGColor(element, red, green, blue);
 			setDisplayValues(red, green, blue);
 		});
 	}
+}
 /*	//                     function param arrow to function
 	rgb.red.addEventListener('change', () => {
 		console.log("Red value:", rgb.red.value);
@@ -38,12 +38,12 @@ function setColorPickerEventListeners(element, colors, pickerElements) {
 		console.log("Blue value:", rgb.blue.value);
 		setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
 
-	});*/
+	});
 
-}
+}*/
 
 function setElementBGColor(bgElement, red, green, blue) {
-	let rbgVal = [red, green, blue].join(',');
+	let rgbVal = [red, green, blue].join(',');
 	bgElement.style.backgroundColor = "rgb(" + rgbVal + ")";
 }
 
